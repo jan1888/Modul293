d72b6658372362d3ba30f79e29878d881d5866ae
jan302 <jan.schaer@icloud.com>
2025-04-01 15:43:54 +0200

    kontaktformular

--- function.js
+++ function.js
@@ -9,9 +9,13 @@ function sendMail() {
   emailjs.send("service_miksfq5", "template_no9rquw", templateParams).then(
     (response) => {
       console.log("SUCCESS!", response.status, response.text);
+      window.location.href = "success.html";
     },
     (error) => {
       console.log("FAILED...", error);
+      alert(
+        "Es gab einen Fehler beim Senden der Nachricht. Bitte versuchen Sie es später erneut."
+      );
     }
   );
 }
