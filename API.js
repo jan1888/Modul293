4ac1e2a22badcc4b6b198545527d8543ee58e693
jan302 <jan.schaer@icloud.com>
2025-04-01 13:05:35 +0200

    commit

--- API.js
+++ API.js
@@ -1,19 +1,12 @@
-import { emailjs } from "@emailjs/browser";
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const serviceID = "service_miksfq5"; // Ersetze "YOUR_SERVICE_ID" mit deiner EmailJS Service ID
-    const templateID = "service_miksfq5"; // Ersetze "YOUR_TEMPLATE_ID" mit deiner EmailJS Template ID
-
-    emailjs.sendForm(serviceID, templateID, this).then(
-      () => {
-        alert("Nachricht erfolgreich gesendet!");
-        this.reset();
-      },
-      (error) => {
-        alert("Fehler beim Senden der Nachricht: " + JSON.stringify(error));
-      }
-    );
-  });
+function sendMail() {
+  let parms = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+    anrede: document.getElementById("geschlecht").value,
+  };
+  return emailjs
+    .send("service_miksfq5", "template_no9rquw", parms)
+    .then((response) => console.log("Email sent successfully:", response))
+    .catch((error) => console.error("Error sending email:", error));
+}
